9109afb214aa25fee05495b11085b7b2989e2277
Ekaterina <kattekly@mail.ru>
2022-11-06 14:45:04 +0300

    Task-8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,24 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            if (action.payload === 'up') {
+                return {
+                    ...state.sort((a, b) => a.age - b.age)
+                }
+            } else {
+                return {
+                    ...state.sort((a, b) => b.age - a.age)
+                }
+            }
         }
         case 'check': {
+            return {
+                ...state.filter(el => el.age > 18)
+            }
 
-            return state // need to fix
         }
         default:
             return state
