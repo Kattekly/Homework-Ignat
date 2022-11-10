75e9a2afa2299e2b6b84b83db7c8a86207578f51
Ekaterina <kattekly@mail.ru>
2022-11-10 18:53:59 +0300

    Task-9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -79,14 +79,14 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={!(start)} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={!!(start)} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop
