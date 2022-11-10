0e50268dc82e01b6ed685581389e2810a827eab9
Ekaterina <kattekly@mail.ru>
2022-11-10 18:43:57 +0300

    Task-9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,8 +33,13 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = (date.getHours()) + ':' + (date.getMinutes()) + ':' + (date.getSeconds()) // пишут студенты
-    const stringDate = ''// день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = new Intl.DateTimeFormat('ru', {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    }).format(date)// пишут студенты*/
+    const stringDate = new Intl.DateTimeFormat('ru').format(date)
+    // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date)
