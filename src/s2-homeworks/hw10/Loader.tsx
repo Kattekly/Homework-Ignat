4288f792bd40110abd2dacb3f6c9d075126a9973
Ekaterina <kattekly@mail.ru>
2022-11-19 13:25:32 +0300

    Task-10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,5 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <img src="https://i.pinimg.com/originals/d4/52/da/d452daacda184e8b3463441c6943d9dd.gif" width="50" height="50"/>
+    /*<div className={s.loader}/>*/
+
