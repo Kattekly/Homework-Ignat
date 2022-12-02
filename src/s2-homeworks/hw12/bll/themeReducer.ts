a88522f596cd6e48777d81a0e10e36441f4ed3d9
Ekaterina <kattekly@mail.ru>
2022-12-02 09:36:09 +0300

    Task-12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,10 +1,23 @@
-const initState = {
+
+type ThemePropsType = {
+    themeId: number
+}
+
+type changeThemeACType = ReturnType<typeof changeThemeId>
+
+const initState: ThemePropsType = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: changeThemeACType): ThemePropsType => { // fix any
     switch (action.type) {
         // дописать
+        case 'SET_THEME_ID': {
+            return {
+                ...state,
+                themeId: action.themeId.find(el => el.themeId === action.themeId)
+            }
+        }
 
         default:
             return state
